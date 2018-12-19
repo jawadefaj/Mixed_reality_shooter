4b6f2e5d197e73d064c336a93212307af1ecbc25
jawadefaj <jawadefaj006@gmail.com>
2018-12-19 10:16:30 -0800

    push

---
+++ MRtest/Assets/testScenes/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public GameObject[] Cube;
+    public GameObject PlayerObject;
+    public float rad;
+	// Use this for initialization
+	void Start () {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(GameObject cube in Cube)
+        {
+            cube.transform.RotateAround(PlayerObject.transform.position, Vector3.up, Random.RandomRange(0.1f, 0.5f));
+        }
+           
+    }
+}
---
+++ MRtest/Assets/testScenes/inputTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputTest : MonoBehaviour {
+    public GameObject sphere;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void DoSome() {
+        sphere.GetComponent<Transform>().position = new Vector3(0, 0, 0);
+    }
+}
